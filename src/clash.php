2e79913655ed67af46f3f4c060d24458e15ab69f
hs <hs96.cn@gmail.com>
2020-05-09 15:58:10 +0800

    set v1

--- src/clash.php
+++ src/clash.php
@@ -1,17 +1,9 @@
 <?php
+
 class clash
 {
-    public function test()
+    public function v1 ()
     {
-        $sql = "SELECT * FROM user WHERE id=1";
-        $str = 'this is v1';
-
-
-        $str = 'this is bc';
-
-        $str = 'this is wqc';
-    }
-    public function more(){
-        //todo wqc
+       //todo
     }
 }
